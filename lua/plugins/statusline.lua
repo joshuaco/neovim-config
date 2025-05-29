ac5c47fbd132247bd0264f98c5af697fb4b26772
Joshua <joxuecont@gmail.com>
2025-05-29 19:05:59 -0400

    Enhance mini.statusline configuration with custom Insert mode styling

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,1 +1,15 @@
-return { 'echasnovski/mini.statusline', version = false, opts = {} } 
+return { 
+  'echasnovski/mini.statusline', 
+  version = false, 
+  opts = {},
+  config = function()
+    require('mini.statusline').setup()
+    
+    -- Personalizar solo el modo Insert
+    vim.api.nvim_set_hl(0, 'MiniStatuslineModeInsert', { 
+      fg = '#1e1e2e',  -- Texto negro/oscuro
+      bg = '#a6e3a1',  -- Fondo verde claro
+      bold = true 
+    })
+  end
+} 
